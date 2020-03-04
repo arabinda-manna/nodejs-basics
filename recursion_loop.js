7e3182230b8073e84667db303b286271b91211f3
Arabinda Manna <arabinda87am@gmail.com>
2020-03-04 11:05:39 +0530

    implemented fully recursive function

--- recursion_loop.js
+++ recursion_loop.js
@@ -1,11 +1,14 @@
 //Print number 1 to 100 without any loop
-let num_counter = 0;
-function print_recursion(){
+
+function print_recursion(num_counter, output=""){
     num_counter++;
-    console.log(num_counter);
+    output = output + " " + num_counter.toString();
     if(num_counter < 100){
-        return print_recursion();
+        return print_recursion(num_counter, output);
+    }else{
+        console.log(output);
     }
+    return false;
 }
 
-print_recursion();
+print_recursion(0, "");
