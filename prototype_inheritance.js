3068bd077e63c14625d6a228648422e547fb84e7
Arabinda Manna <arabinda87am@gmail.com>
2020-03-16 18:23:01 +0530

    prototype inheritance done with array storing

--- prototype_inheritance.js
+++ prototype_inheritance.js
@@ -3,8 +3,9 @@ function Animal(age, name){
     this.name = name;
 }
 
-function Human(age, name) {
+function Human(age, name, leg) {
    Animal.call(this, age, name);
+   this.legs = leg;
 }
 
 Object.defineProperties(Human.prototype, {
@@ -14,8 +15,9 @@ Object.defineProperties(Human.prototype, {
     }
 });
 
-function Bird(age, name) {
+function Bird(age, name, wing) {
     Animal.call(this, age, name);
+    this.wings = wing;
 }
 
 Object.defineProperties(Bird.prototype, {
@@ -25,8 +27,9 @@ Object.defineProperties(Bird.prototype, {
     }
 });
 
-function Reptile(age, name) {
+function Reptile(age, name, tail) {
     Animal.call(this, age, name);
+    this.tails = tail;
 }
 
 Object.defineProperties(Reptile.prototype, {
@@ -36,16 +39,23 @@ Object.defineProperties(Reptile.prototype, {
     }
 });
 
-let human = new Human(24, "Arabinda Manna");
-human.legs = 2;
+let array = [];
+array.push(new Human(24, "Arabinda Manna", 2));
+array.push(new Bird(1, "Parrot", 2));
+array.push(new Human(28, "Abhisek Kundu", 2));
+array.push(new Bird(10, "Eagle", 2));
+array.push(new Reptile(20, "Python", 1));
+array.push(new Bird(5, "Peacock", 2));
+array.push(new Human(27, "Srijita Thakur", 2));
+array.push(new Reptile(11, "Crocodile", 1));
+array.push(new Human(34, "Rajashri Ghosh", 2));
 
-let bird = new Bird(1, "Parrot");
-bird.wings = 2;
-
-let reptile = new Reptile(1, "Python");
-reptile.tails = 1;
-
-
-console.log(human);
-console.log(bird);
-console.log(reptile);
+for (const value of array) {
+    if(value instanceof Human){
+        console.log("Human Name : " + value.name + " & Age : " + value.age + " & No of Legs : " + value.legs);
+    } else if (value instanceof Bird){
+        console.log("Bird Name : " + value.name + " & Age : " + value.age + " & No of Wings : " + value.wings);
+    } else if (value instanceof Reptile){
+        console.log("Reptile Name : " + value.name + " & Age : " + value.age + " & No of Tails : " + value.tails);
+    }
+}
