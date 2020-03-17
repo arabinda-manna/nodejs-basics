a74b1be5e5f298d948e182cfa06135a40ba53691
Arabinda Manna <arabinda87am@gmail.com>
2020-03-17 11:39:12 +0530

    own json stringify method implemented

--- json_object_print.js
+++ json_object_print.js
@@ -1,6 +1,58 @@
-function print_json(obj) {
-    console.log(JSON.stringify(obj));
+function print_json(obj, key = null, index) {
+    // console.log(obj);
+    // console.log(typeof obj);
+    const commaPresent = (index > 0 ? "," : "")
+
+    if (typeof obj === "string") {
+        if(key){
+            return commaPresent + "\"" + key + "\"" + ":" + "\"" + obj + "\"";
+        }else{
+            return commaPresent + obj;
+        }
+    } else if (typeof obj === "number") {
+        if (key) {
+            return commaPresent + "\"" + key + "\"" + ":" + obj;
+        } else {
+            return commaPresent + obj;
+        }
+    } else if (obj === null) {
+        if (key) {
+            return commaPresent + "\"" + key + "\"" + ":" + "null";
+        } else {
+            return commaPresent + "null";
+        }
+    }else if (typeof obj === "object"){
+        if(obj instanceof Array){
+            let returnData = "[";
+            for (let i = 0; i < obj.length; i++) {
+                returnData += print_json(obj[i], null, i);   
+            }
+            returnData += "]"
+            if (key) {
+                return commaPresent + "\"" + key + "\"" + ":" + returnData;
+            } else {
+                return commaPresent + returnData;
+            }
+        }else if(obj instanceof Object){     
+            let returnData = "{";
+            let i = 0;
+            for(let key in obj) {
+                returnData += print_json(obj[key], key, i);
+                i++;
+            }
+            returnData += "}";
+
+            if (key) {
+                return commaPresent + "\"" + key + "\"" + ":" + returnData;
+            } else {
+                return commaPresent +  returnData;
+            }
+        }
+    }else{
+        //undefined case
+        return "";
+    }
 }
 
-let object = {"firstName":"Arabinda", "lastName":"Manna", "age":25};
-print_json(object);
+let object = { "firstName": "Arabinda", "lastName": "Manna", "age": 25, "height": null, "weight": undefined, "chest": [56,38,null], null: null, abc: "abc" };
+console.log(print_json(object));
