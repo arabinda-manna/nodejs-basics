e2e40e9894752edde82630cbc09a83f2e5609976
Arabinda Manna <arabinda87am@gmail.com>
2020-03-24 09:28:23 +0530

    operation controller test cases implemented

--- simple-express-server/test/controllers/operationsTest.js
+++ simple-express-server/test/controllers/operationsTest.js
@@ -27,3 +27,53 @@ describe("processFirst10Multiples Controller Function Unit Testing", () => {
         done();
     });
 });
+
+describe("processStringCharacterCalc Controller Function Unit Testing", () => {
+    it("next should be called", async (done) => {
+        sinon.stub(operations, "stringCharacterCalc").throws(new Error("stringCharacterCalc throws wanted error"));
+        const sinonNext = sinon.spy();
+        processStringCharacterCalc({}, {}, sinonNext);
+        expect(sinonNext.calledOnce).to.be.true;
+        operations.stringCharacterCalc.restore();
+        done();
+    });
+
+    it("res.send should be called", async (done) => {
+        const req = {"body":{
+            "string":"NodeJs"
+        }};
+        const sendFake = sinon.spy();
+        const resSpy = {
+            "send": sendFake
+        };
+        processStringCharacterCalc(req, resSpy, () => { });
+        expect(sendFake.calledOnce).to.be.true;
+        done();
+    });
+});
+
+describe("processAmstrong Controller Function Unit Testing", () => {
+    it("next should be called", async (done) => {
+        sinon.stub(operations, "isAmstrong").throws(new Error("isAmstrong throws wanted error"));
+        const sinonNext = sinon.spy();
+        processAmstrong({}, {}, sinonNext);
+        expect(sinonNext.calledOnce).to.be.true;
+        operations.isAmstrong.restore();
+        done();
+    });
+
+    it("res.send should be called", async (done) => {
+        const req = {
+            "params": {
+                "num": 153
+            }
+        };
+        const sendFake = sinon.spy();
+        const resSpy = {
+            "send": sendFake
+        };
+        processAmstrong(req, resSpy, () => { });
+        expect(sendFake.calledOnce).to.be.true;
+        done();
+    });
+});
